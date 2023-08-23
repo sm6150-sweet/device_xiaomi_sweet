8aa424ad2e749176c9c5f6e23445bee92c64e83b
Niranjan BR <niranjankannan2003@gmail.com>
2023-08-23 22:25:29 +0000

    Revert "sm6150-common: Drop Anti flicker & SunlightEnhancement"
    This reverts commit 883e9cc23ff4e8a19bfdabe2b7f74c72c2117670.

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -19,21 +19,27 @@
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
-#include <livedisplay/sdm/PictureAdjustment.h>
 
+#include "AntiFlicker.h"
+#include "SunlightEnhancement.h"
 
-using ::vendor::lineage::livedisplay::V2_0::sdm::PictureAdjustment;
-using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
+using ::vendor::lineage::livedisplay::V2_1::IAntiFlicker;
+using ::vendor::lineage::livedisplay::V2_1::ISunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_1::implementation::AntiFlicker;
+using ::vendor::lineage::livedisplay::V2_1::implementation::SunlightEnhancement;
 
 int main() {
-
-    std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
-    android::sp<PictureAdjustment> pictureAdjustment = new PictureAdjustment(controller);
+    android::sp<IAntiFlicker> antiFlicker = new AntiFlicker();
+    android::sp<ISunlightEnhancement> sunlightEnhancement = new SunlightEnhancement();
 
     android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
 
-    if (pictureAdjustment->registerAsService() != android::OK) {
-        LOG(ERROR) << "Cannot register picture adjustment HAL service.";
+    if (antiFlicker->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register anti flicker HAL service.";
+        return 1;
+    }
+    if (sunlightEnhancement->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register sunlight enhancement HAL service.";
         return 1;
     }
 
