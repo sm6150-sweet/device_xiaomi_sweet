409b9dd785a274281972dd9309c79bc86a31b4c9
Bruno Martins <bgcngm@gmail.com>
2024-05-25 07:47:57 +0000

    sweet: Use common libqti-perfd-client and power-libperfmgr
    Signed-off-by: Alen24 <alenbijuk2003@gmail.com>
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -220,6 +220,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
+include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
