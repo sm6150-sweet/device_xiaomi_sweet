0ade4f309cf694aace40b486425464c5bff51737
Bruno Martins <bgcngm@gmail.com>
2024-05-25 07:47:57 +0000

    sweet: Use common libqti-perfd-client and power-libperfmgr
    Signed-off-by: Alen24 <alenbijuk2003@gmail.com>
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -403,7 +403,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     android.hardware.power.stats@1.0-service.mock 
 
 PRODUCT_COPY_FILES += \
@@ -473,7 +473,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client 
 
 # Telephony
 PRODUCT_PACKAGES += \
