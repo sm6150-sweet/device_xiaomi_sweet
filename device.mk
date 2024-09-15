4c4a568825ce7cb02a141a6e40b629f990bc4f8e
Niranjan BR <niranjankannan2003@gmail.com>
2024-09-15 06:43:37 +0000

    sweet: Remove redundant fingerprint service
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -189,9 +189,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sm6150
-    
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
